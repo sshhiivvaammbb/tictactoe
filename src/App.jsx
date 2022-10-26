c8118e2ca494587c3a704e1636c03c1a6d4203df
shivam bhardwaj <shivambhardwaj722@gmail.com>
2022-10-26 23:08:35 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+
+    <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hii Shivam Bhardwaj . Welcome to your first react project</p>
+  </>
+  )
+}
+
+export default App;
+  
+
