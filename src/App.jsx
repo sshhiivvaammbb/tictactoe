f05612c8ae6bddc4cf05e3ff5c10d60ceef25527
shivam bhardwaj <shivambhardwaj722@gmail.com>
2022-10-27 12:22:36 +0530

    board and sqaure files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Board from './components/Board'
 
 const App = () => {
   return (
 
-    <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hii Shivam Bhardwaj . Welcome to your first react project</p>
-  </>
+    <div>
+    <h1>TIC TAC TOE</h1>
+    <Board/>
+    
+  </div>
   )
 }
 
